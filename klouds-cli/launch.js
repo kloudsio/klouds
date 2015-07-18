0145c409b751b6403d3d42877641297ebf9f59d3
Daniel Sont <dan.sont@gmail.com>
2015-07-18 07:02:42 +0700

    klouds-cli

--- klouds-cli/launch.js
+++ klouds-cli/launch.js
@@ -1,55 +1,36 @@
 import arg3r from 'minimist'
 import env from './env.json'
-import fs from 'fs'
 import { join } from 'path'
 import { spawn } from 'child_process'
 
 let argv = arg3r(process.argv.slice(2))
 let targets = argv._
 
-
-
 console.log(`klouds-run on ${ targets.length } targets`)
 
-
-function scriptPath(name) {
-  return join(__dirname, `apps/${name}/${name}.sh`)
-}
-
 function launch(name) {
-  let script = scriptPath(name)
-  console.log('spawning:', script)
+  console.log('spawning:', name)
 
-  let cp = spawn('bash', [script], {
-    cwd: join(__dirname, 'apps', name),
-    env,
-    ...process.env
+  let file = name + '.sh'
+  let cwd = join(__dirname, `apps/${name}`)
+
+  let cp = spawn('bash', [ file ], {
+    cwd,
+    env: {...env, ...process.env },
+    stdio: 'inherit'
   })
 
-  cp.stdout.pipe(process.stdout)
-  cp.stderr.pipe(process.stderr)
-  process.stdin.pipe(cp.stdin)
-
-  cp.on('error', function (err) {
-    console.error('Error executing launcher', script, err)
+  cp.stdout.on('data', function (data) {
+    console.log(name + ': ' + data)
   })
-}
 
-function exists(x) {
-  return new Promise((res, rej) => {
-    fs.exists(scriptPath(x), mmhm => {
-      if (mmhm) {
-        return res(x)
-      } else {
-        rej(x)
-      }
-    })
+  cp.stderr.on('data', function (data) {
+    console.error(name + 'ERR! ' + data)
+  })
+
+  cp.on('close', function (code) {
+    console.log('child process exited with code ' + code)
   })
 }
 
-
-for (let name of targets) {
-  let ex = scriptPath(name)
-  exists(ex).then(launch).catch(console.error)
-}
-
+targets.map(launch)
