fb84289c824e9ad5b4dccb846cd755bcf36aff46
Daniel Sont <dan.sont@gmail.com>
2015-07-27 10:54:35 +0700

    Launchage Happenned

--- server/rancher/compose.js
+++ server/rancher/compose.js
@@ -3,28 +3,42 @@
  */
 
 import extend from 'extend'
-import { join } from 'path'
-import { spawn } from 'child_process'
+import {join} from 'path'
+import {exec} from 'child_process'
 
-import config from '../config'
+import {rancher} from '../config'
+
+
+/**
+ * Run rancher-compose
+ */
+function compose(options) {
+  function log(err, stdout, stderr) {
+    console.log(stdout)
+    console.error(stderr)
+    if (err !== null) {
+      console.error('exec err: ' + err)
+    }
+  }
+  return subcommand => () => exec(`rancher-compose ${rancher} ${subcommand}`, options, log)
+}
 
 /**
  * Creates an interface for an app
  * app('wordpress')
  */
-function appCompose(path) {
-  let spawnOptions = extend(config.rancher, {
+function appInterface(path) {
+  let options = {
     cwd: join(__dirname, path)
-  })
-  console.dir(spawnOptions)
-  let composeCmd = cmd => () => spawn(`rancher-compose`, [ cmd ], spawnOptions)
+  }
+  let appCompose = compose(options)
   return {
-    up: composeCmd('up'),
-    down: composeCmd('down'),
-    restart: composeCmd('restart')
+    up: appCompose('up -d'),
+    down: appCompose('down'),
+    restart: appCompose('restart')
   }
 }
 
 export default {
-  wordpress: appCompose('./wordpress')
+  wordpress: appInterface('./wordpress')
 }
