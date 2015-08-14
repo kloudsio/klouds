2c86e956179370765a0bdc8800b9a0a81cf40a30
Daniel Sont <dan.sont@gmail.com>
2015-08-14 11:49:39 +0700

    Added Docker Compose for Klouds

--- kloud-compose/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  url: 'http://ds0nt.com:8080/v1/projects/1a5',
-  projectname: 'klouds',
-  accesskey: '959E2C95C8C1552A63DE',
-  secretkey: 'mHHA3YXKxRXFxLfDiWuh9sbDecEoNcxjpZvwZSeq'
-}
